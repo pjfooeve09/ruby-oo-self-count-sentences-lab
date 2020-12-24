4ac287ce457001a24d708ebe1b36d0b94026edff
Parham Javadi <pjfooeve09@yahoo.com>
2020-12-24 15:40:07 -0800

    commit tree 91d7fcc7b3ced4ff2b307863587c2e7d358862d4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,34 @@
-require 'pry'
-
 class String
+  
+  attr_accessor 
 
   def sentence?
-    
+    if self.end_with?('.')
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!|\!!|\.../).delete_if { |word| word.size < 2}.size
+  
   end
+
 end
